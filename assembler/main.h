056cf4784d3f8da5e535a9018e18e09708cbf0a7
trexxet <atomnik07@gmail.com>
2016-04-24 02:26:01 +0300

    Initial commit

---
+++ assembler/main.h
@@ -0,0 +1,8 @@
+#define SOURCE_STRING_LENGTH 64 //Max length of text string in source file
+#define INIT_PROGRAM_SIZE 128 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "asm.h"
+#include "errors.h"
