f39a016ed920132f299961882e0c93a4756863cf
trexxet <atomnik07@gmail.com>
2020-07-02 04:04:54 +0300

    Allow sequential parse of expr's

--- assembler/eval/test.c
+++ assembler/eval/test.c
@@ -3,25 +3,43 @@
 #include <inttypes.h>
 #include "eval.tab.h"
 
-extern YYLTYPE yylloc;
-extern int yy_scan_string(char *);
-extern int yylex_destroy();
+
+static char* expr = NULL;
 
 
-static char* expr = "0x3+4*2+1";
+YYSTYPE evalExpr(char *expr, int* err) {
+	extern int yy_scan_string(char *);
+	extern void lex_reset_state();
+	lex_reset_state();
+	yy_scan_string(expr);
+	YYSTYPE result = 0;
+	*err = yyparse(&result);
+	return result;
+}
+
+
+void evalPrintExpr(char *expr) {
+	int err = 0;
+	YYSTYPE result = evalExpr(expr, &err);
+	if (!err) printf("%s = %" YYSTYPE_PR "\n", expr, result);
+}
 
 
 int main() {
-	yy_scan_string(expr);
-	YYSTYPE result = 0;
-	yyparse(&result);
+	expr = "0x3+4*2+1";
+	evalPrintExpr(expr);
+	expr = "1 + 2";
+	evalPrintExpr(expr);
+	expr = "0xA + 2";
+	evalPrintExpr(expr);
+	extern int yylex_destroy();
 	yylex_destroy();
-	printf("Result: %" YYSTYPE_PR "\n", result);
 	return 0;
 }
 
 
 int yyerror(YYSTYPE* res, char const *s) {
+	extern YYLTYPE yylloc;
 	printf("%s\n", expr);
 	for (int i = 0; i < yylloc.first_column - 1; i++) putchar(' ');
 	printf("^\n");
